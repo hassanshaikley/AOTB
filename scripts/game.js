89685eb18cbbd68e965063ca685d10d79f55bc4f
Hassan S <hassan.shaikley@gmail.com>
2014-08-12 13:50:49 -0700

    ia url works better heroku

--- scripts/game.js
+++ scripts/game.js
@@ -34,7 +34,7 @@ function init() {
   localPlayer = new Player(startX, startY);
 
   // Initialise socket connection
-  socket = io.connect("http://amara.herokuapp.com/", {port: PORT, transports: ["websocket"]});
+  socket = io.connect(APP_URL, {port: PORT, transports: ["websocket"]});
 
   // Initialise remote players array
   remotePlayers = [];
