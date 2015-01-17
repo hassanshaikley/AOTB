0beeffd48789e66276bfbd61d332d715ea4325c8
Hassan Shaikley <hassan.shaikley@gmail.com>
2015-01-16 21:53:49 -0800

    refactored fly

--- fly.js
+++ fly.js
@@ -4,40 +4,22 @@
 Player = require("./player").Player;
 var util = require("util");
 var Fly = function(x, y, hp, name){
-  var fly =  Player(x, y, hp, name);
+  var skeleton =  Player(x, y, hp, name);
   var descendAttack = false;
 
-  var setDescendAttack = function(boolean_thing){
+  skeleton.setDescendAttack = function(boolean_thing){
     descendAttack = boolean_thing;
   };
 
-  /* Descend attack is the Flys signature move 
-   * While true it damages players it crosses!
-   */
-  var getDescendAttack = function(){
+  skeleton.getDescendAttack = function(){
     return descendAttack;
   };
 
-  var getCharacterType = function(){
+  skeleton.getCharacterType = function(){
     return "Fly";
   };
 
-  return {
-    getX : fly.getX,
-         getY : fly.getY,
-         setX : fly.setX,
-         setY : fly.setY,
-         getHp : fly.getHp,
-         setHp : fly.setHp,
-         setName : fly.setName,
-         getName : fly.getName,
-         getCharacterType : getCharacterType,
-         id: fly.id,
-         setDescendAttack : setDescendAttack,
-         getDescendAttack : getDescendAttack,
-         getZone : fly.getZone,
-         setZone : fly.setZone
-  };
+  return skeleton;
 };
 
 exports.Fly = Fly;
