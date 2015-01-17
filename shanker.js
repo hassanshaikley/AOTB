53c2b03c652f1ad6ad1970324f1d3ebf6ee972a6
Hassan Shaikley <hassan.shaikley@gmail.com>
2015-01-16 22:07:29 -0800

    refactor

--- shanker.js
+++ shanker.js
@@ -7,25 +7,11 @@ var util = require("util");
 var Shanker = function(x, y, hp, name){
   var skeleton = Player(x, y, hp, name);
 
-  var getCharacterType = function(){
+  skeleton.getCharacterType = function(){
     return "Shanker";
   };
 
-  return {
-    getX : skeleton.getX,
-         getY : skeleton.getY,
-         setX : skeleton.setX,
-         setY : skeleton.setY,
-         getCharacterType : getCharacterType,
-         getHp : skeleton.getHp,
-         setHp : skeleton.setHp,
-         setName : skeleton.setName,
-         getName : skeleton.getName,
-         setZone : skeleton.setZone,
-         id: skeleton.id,
-         getZone : skeleton.getZone,
-         setZone : skeleton.setZone
-  };
+  return skeleton;
 };
 
 exports.Shanker = Shanker;
