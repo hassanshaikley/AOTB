0c41f712de9bb2c8b99eb9a7e4c9527965f9e086
Hassan Shaikley <hassan.shaikley@gmail.com>
2015-04-08 01:20:26 -0700

    small tests

--- test/units.js
+++ test/units.js
@@ -11,10 +11,28 @@ describe("Swag", function() {
     before(function(){
         var game = new Game();
     });
+
+  
     describe("fly", function() {
         it("should make a fly", function(){
             var fly = new Fly("hassan", 1);
             assert.equal(fly.getName(), "hassan");
+        });
+    });
+
+    
+    describe("shanker", function() {
+        it("should make a shanker", function(){
+            var shanker = new Shanker("hassan", 1);
+            assert.equal(shanker.getName(), "hassan");
+        });
+    });
+      
+
+    describe("redhatter", function() {
+        it("should make a redhatter", function(){
+            var redhatter = new Redhatter("hassan", 1);
+            assert.equal(redhatter.getName(), "hassan");
         });
     });
 });
