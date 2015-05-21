0eaa60d72212d0bc5074150d67686faab8722072
Hassan Shaikley <hassan.shaikley@gmail.com>
2015-05-20 17:28:04 -0700

    added shrines to backgorund

--- scripts/background.js
+++ scripts/background.js
@@ -1,20 +1,23 @@
 function Background() {
   var texture = PIXI.Texture.fromImage("cobblestone_ground.png");
-  
+  	//left bound = 1000 - 768 /2
+  	// length
+  	//right bound = 4000 + 768/2
+
   PIXI.TilingSprite.call(this, texture, 4000 + Math.abs(768/2), 100); //repeats for 400 x
-  this.position.x = 100-Math.abs(768/2); 
+  this.position.x = 1000-Math.abs(768/2); 
   this.position.y = 370;
   this.tilePosition.x = 50;
   this.tilePosition.y = 0 ;
  
 
-  var structure = new PIXI.Sprite(PIXI.Texture.fromImage("spire.png"));
-  structure.x = 1250;
+  structure = new PIXI.Sprite(PIXI.Texture.fromImage("spire.png"));
+  structure.x = 1350 - Math.abs(PIXI.Texture.fromImage("spire_0.png").width/2);
   structure.y = -100;
 
-    var structure2 = new PIXI.Sprite(PIXI.Texture.fromImage("spire_0.png"));
-  structure.x = 3750;
-    structure2.y = -100;
+  structure2 = new PIXI.Sprite(PIXI.Texture.fromImage("spire_0.png"));
+  structure2.x = 3650 - Math.abs(PIXI.Texture.fromImage("spire_0.png").width/2);
+   structure2.y = -100;
 
   this.addChild(structure);
   this.addChild(structure2);
@@ -28,7 +31,8 @@ Background.prototype = Object.create(PIXI.TilingSprite.prototype);
 
 Background.prototype.setX = function(newX) {
 //  var distanceTravelled = newViewportX - this.viewportX;
-    var displacement = -localPlayer.getDrawAtX() + Math.abs(768/2);
+  //  var displacement = -localPlayer.getDrawAtX() + Math.abs(768/2);
+    var displacement = canvas.width/2- localPlayer.getDrawAtX();
 
   this.position.x = displacement;
 
