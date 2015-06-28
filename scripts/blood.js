bc0c0541449a1abc2c6afe4313b4dbabddaf3d2d
hassan shaikley <hassan.shaikley@gmail.com>
2015-06-28 03:43:10 -0700

    animates blood

--- scripts/blood.js
+++ scripts/blood.js
@@ -6,22 +6,36 @@ var Blood = function(startX, startY) {
       y =               startY,
       animate = 0;
 
- 
+ var blood0 = new PIXI.Sprite(PIXI.Texture.fromImage("blood0.png")); 
+ var blood1 = new PIXI.Sprite(PIXI.Texture.fromImage("blood1.png")); 
+ var blood2 = new PIXI.Sprite(PIXI.Texture.fromImage("blood2.png")); 
+ var blood3 = new PIXI.Sprite(PIXI.Texture.fromImage("blood3.png")); 
+blood0.y = blood1.y = blood2.y = blood3.y = y;
   var draw = function(){
-    animate++;
     var   drawAtX = CONFIG.SCREEN_WIDTH/2 + x - localPlayer.getDrawAtX() ;
-    console.log("DRAWLE BLOOD -- " +drawAtX) ;
-    if (animate  < 10 ){
-      ctx.drawImage(blood,0,0, 100, 100,  drawAtX, y, 100, 100);
-    } else if (animate < 20){
-      ctx.drawImage(blood,100,0, 100, 100,  drawAtX, y, 100, 100);
-    } else if (animate < 30){
-      ctx.drawImage(blood,200,0, 100, 100,  drawAtX, y, 100, 100);
-    } else if ( animate < 40) {
-      ctx.drawImage(blood,300,0, 100, 100,  drawAtX, y, 100, 100);
-    } else {
+   blood0.x = blood1.x = blood2.x = blood3.x = drawAtX;
+	console.log("GOING " + animate);
+    if (animate  === 0 ){
+	console.log("1");
+	MAIN.stage.addChild(blood0);
+    } else if (animate == 5){
+	console.log("2");
+	MAIN.stage.removeChild(blood0);
+	MAIN.stage.addChild(blood1);
+    } else if (animate ==10){
+	console.log("3");
+	MAIN.stage.removeChild(blood1);
+	MAIN.stage.addChild(blood2);
+    } else if ( animate ==15) {
+	console.log("4");
+	MAIN.stage.removeChild(blood2);
+	MAIN.stage.addChild(blood3);
+    } else if ( animate == 20) {
+	console.log("5");
+	MAIN.stage.removeChild(blood3);
       bloods.shift();
     }
+    animate+= 1;
   };
 
   return {
